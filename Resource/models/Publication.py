c7d3f494d5b8630845a69f275df997e0b36b2ce7
vaibhav sawhney <returnofmax@gmail.com>
2016-04-16 13:07:50 +0530

    Changes in Resource.models
    Minor changes in resource models

--- Resource/models/Publication.py
+++ Resource/models/Publication.py
@@ -1,11 +1,10 @@
 from django.db import models
 
+
 class Publication(models.Model):
-	publicationId = models.CharField(max_length=20, primary_key=True, blank=False, null=False)
-	title = models.CharField(max_length=50, blank=False, null=False)
-	authors = models.CharField(max_length=50)
-	publicationDate = models.DateField()
-	organization = models.CharField(max_length=50)
-	webLink = models.URLField(max_length=200)
-	
-	
+    publicationId = models.CharField(max_length=20, primary_key=True, blank=False, null=False)
+    title = models.CharField(max_length=150, blank=False, null=False)
+    authors = models.CharField(max_length=70)
+    publicationDate = models.DateField(editable=True, auto_now=False, auto_now_add=False)
+    organization = models.CharField(max_length=70)
+    webLink = models.URLField(max_length=256)
